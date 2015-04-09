2aa4f0bcb051709f4749b8a7d437c99d3e3d6f95
root <root@ip-172-31-20-137.us-west-2.compute.internal>
2015-04-09 16:28:45 +0000

    Change endpoint url to match Ensembl service

--- webui/urls.py
+++ webui/urls.py
@@ -5,8 +5,8 @@ from webui import views
 
 urlpatterns = patterns('',
     url(r'^$', views.showapi, name='index'),
-    url(r'^lookup/symbol/(?P<symbol>\w+)/(?P<species>\w+)/$', views.symbolbyname, name='symbolbyname'),
-    url(r'^lookup/symbol/(?P<symbol>\w+)/$', views.symbolbyname, name='symbolbyname'),
+    url(r'^xrefs/name/(?P<symbol>\w+)/(?P<species>\w+)/$', views.symbolbyname, name='symbolbyname'),
+    url(r'^xrefs/name/(?P<symbol>\w+)/$', views.symbolbyname, name='symbolbyname'),
     url(r'^xrefs/symbol/(?P<symbol>\w+)/(?P<species>\w+)/$', views.idbysymbol, name='idbysymbol'),
     url(r'^xrefs/symbol/(?P<symbol>\w+)/$', views.idbysymbol, name='idbysymbol'),
     url(r'^lookup/id/(?P<id>\w+)/$', views.moleculebyid, name='moledulebyid'),
